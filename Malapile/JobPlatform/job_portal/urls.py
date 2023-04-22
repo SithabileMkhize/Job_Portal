d058966e31e8ec655b46e1045b56dbb967974970
Malapilesa <126823252+Malapilesa@users.noreply.github.com>
2023-04-22 21:13:34 +0200

    Delete Malapile/JobPlatform directory

--- Malapile/JobPlatform/job_portal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.main, name='main'),
-    path('home/', views.home, name='HomePage'),
-    path('home/addNew/', views.addNewJob, name='AddNew'),
-    path('home/jobInfo/<int:id>', views.jobInfo, name='JobsInfo')
-]
